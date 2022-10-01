71a32f6f948a3d8b5d79459d0f8ba76a690599dd
shah4 <shah4@shah4corp>
2022-10-01 13:18:24 +0530

    edited in eclipse by shahnawaz

--- shahnawaz4automation/src/test/java/seleniumPractice/WelcomeMessage.java
+++ shahnawaz4automation/src/test/java/seleniumPractice/WelcomeMessage.java
@@ -3,7 +3,8 @@ package seleniumPractice;
 public class WelcomeMessage {
 	public static void main(String[] args) {
 		System.out.println("Mohammed Shahnawaz");
-		System.out.println("printout command added in git")
+		System.out.println("printout command added in git");
+		System.out.println("New message added for testing");
 	}
 
 }
