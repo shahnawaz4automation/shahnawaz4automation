5073245a3c5e877d62451e1a24391097376796d2
Mohammed Shahnawaz <48588547+shahnawaz4automation@users.noreply.github.com>
2022-10-01 13:15:54 +0530

    Commited in git for testing

--- shahnawaz4automation/src/test/java/seleniumPractice/WelcomeMessage.java
+++ shahnawaz4automation/src/test/java/seleniumPractice/WelcomeMessage.java
@@ -3,6 +3,7 @@ package seleniumPractice;
 public class WelcomeMessage {
 	public static void main(String[] args) {
 		System.out.println("Mohammed Shahnawaz");
+		System.out.println("printout command added in git")
 	}
 
 }
