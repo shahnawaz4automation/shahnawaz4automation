aa577883fbee22ef059a5b3a5d22d906a2cd7b18
shah4 <shah4@shah4corp>
2022-10-04 23:11:37 +0530

    commit 04102022

---
+++ shahnawaz4automation/src/main/java/javaPractice/testClass.java
@@ -0,0 +1,15 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class testClass {
+	public void ifelsetestmethod() {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a = scan.nextInt();
+			System.out.println(a);
+		}
+		
+
+	}
+
+}
